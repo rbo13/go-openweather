bd84c8e5259fe1ae9a59e7c7cc4ae12effaa7b5b
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-22 22:14:19 +0800

    Added test for getting a weather from a specific city id
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- openweather_test.go
+++ openweather_test.go
@@ -7,8 +7,9 @@ import (
 	openweather "github.com/whaangbuu/go-openweather"
 )
 
+var apiKey string = os.Getenv("OPENWEATHER_API_KEY")
+
 func TestGetByCityName(t *testing.T) {
-	apiKey := os.Getenv("OPENWEATHER_API_KEY")
 	client := openweather.NewClient(apiKey)
 
 	weatherData, err := client.GetWeatherByCityName("London")
@@ -19,6 +20,21 @@ func TestGetByCityName(t *testing.T) {
 
 	if weatherData == nil {
 		t.Errorf("Weather Data is nil")
+	}
+
+	t.Log(weatherData)
+}
+
+func TestGetByCityID(t *testing.T) {
+	client := openweather.NewClient(apiKey)
+	weatherData, err := client.GetWeatherByCityID(2172797)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if weatherData == nil {
+		t.Errorf("Weather Data i nil")
 	}
 
 	t.Log(weatherData)
